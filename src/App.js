0baa087c53d9cb1fefb5ac001bb0ee1b1cbf8eb4
neilprado <neiljohn17@gmail.com>
2019-03-20 23:08:41 -0300

    Inserting about page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react';
+import {BrowserRouter as Router} from 'react-router-dom'
+import Header from './components/layout/Header'
 import './App.css';
-import Todos from './components/Todos.js';
+import Todos from './components/Todos';
+import AddTodo from './components/AddTodo';
+import About from './components/pages/About';
+import uuid from 'uuid'
+
 
 class App extends Component {
   state = {
     todos: [
       {
-        id: 1,
+        id: uuid.v4(),
         title: 'Take out the Trash',
         completed: false
       },
       {
-        id: 2,
+        id: uuid.v4(),
         title: 'Dinner with wife',
         completed: false
       },
       {
-        id: 3,
+        id: uuid.v4(),
         title: 'Meeting with boss',
         completed: false
       },
@@ -33,11 +39,39 @@ class App extends Component {
       })
     });
   }
+
+  delTodo = (id) => {
+    this.setState({
+      todos: [...this.state.todos.filter(todo => todo.id !== id)]
+    })
+  }
+
+  addTodo = (title) => {
+    const newTodo = {
+      id: uuid.v4(),
+      title: title,
+      completed: false
+    }
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
+  }
   render() {
     return (
-      <div className="App">
-        <Todos todos={this.state.todos} markComplete={this.markComplete}/>
-      </div>
+      <Router>
+        <div className="App">
+          <div className="container">
+            <Header />
+            <Router path="/" render={props => (
+              <React.Fragment>
+                <AddTodo addTodo={this.addTodo}/>
+                <Todos todos={this.state.todos} markComplete={this.markComplete} delTodo={this.delTodo}/> 
+              </React.Fragment>
+            )} />
+            <Router path="/about" Component={About} />
+          </div>
+        </div>
+      </Router>
     );
   }
 }
---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is the ToDo List app, as a crash course from Traversy Media</p>
+    </React.Fragment>
+  )
+}
+
+export default About
